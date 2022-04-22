9e7cec6f79a6a17f958fa90e262f198588515968
amariche <insigned12@gmail.com>
2022-04-22 11:05:46 +0200

    feat TodoList screen

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import Insta from './assets/instagram.png';
+import TodoList from './src/TodoList';
 
 
 export default function App() {
   
   return (
     <View style={styles.container}>
-      <Image source={Insta} style={{width:50, height:50, top:10}}/>
-      <Text style={styles.title}>Bien venu sur instagram</Text>
       <StatusBar style="auto" />
+
+      {/* <Image source={Insta} style={{width:50, height:50, top:10}}/> */}
+      {/* <Text style={styles.title}>Bien venu sur instagram</Text> */}
+
+      <TodoList />
     </View>
   );
 }
@@ -24,6 +28,6 @@ const styles = StyleSheet.create({
   title:{
     alignSelf:'center',
     paddingVertical:30,
-    fontWeight:500,
+    fontWeight:'500', // fontWeight:'500'
   },
 });
---
+++ src/TodoList.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const TodoList = () => {
+  return (
+    <View>
+      <Text>TodoList</Text>
+    </View>
+  )
+}
+
+export default TodoList    
